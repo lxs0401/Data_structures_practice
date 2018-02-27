2bc6df0da107190ca0647dfa442c7c81564df5e0
NhidA <1006955418@qq.com>
2018-02-27 09:36:49 +0800

    2018/2/27 09:36

--- 部分算法/最大字段和.c
+++ 部分算法/最大字段和.c
@@ -1,1 +1,28 @@
+#include <stdlib.h>
+#include<stdio.h>
+int main()
+{
+    int count;
+    int a[100];
+    int b[100];
+    int i;
+    int max;
+    scanf("%d",&count);
+    for(i=0; i<count; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    b[0]=a[0];
+    max=b[0];
+    for(i=1; i<count; i++)
+    {
+        if(b[i-1]>0)
+            b[i]=b[i-1]+a[i];
+        else
+            b[i]=a[i];
+        if(b[i]>max)
+            max=b[i];
+    }
+    printf("%d\n",max);
+    return 0;
 }
