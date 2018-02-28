5bbe2aa4cd67cf92f52e448f27992786ea92c1d4
NhidA <1006955418@qq.com>
2018-02-28 12:31:44 +0800

    re sort

---
+++ 部分算法/全排列.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <malloc.h>
+
+int q_paixu(char *size, int x, char *alpha, int max)
+{
+	int num = 0;
+	if (num < max)
+	{
+		int cha;
+
+		while ((num < max) && (alpha[num] == size[x - 1]))
+		{
+			alpha[num] = size[0];
+			num++;
+		}
+		if (num == max){
+			return 0;
+		}
+		for (cha = 0; (cha < x) && (alpha[num] != size[cha]); cha++)
+			;
+
+		if ((cha < x) && (alpha[num] == size[cha])){
+			alpha[num] = size[cha + 1];
+		}
+		else{
+			return 0;
+		}
+		return 1;
+	}
+	else
+		return 0;
+}
+
+int main()
+{
+	char x[37] = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890", *a;
+	a = (char *)malloc(sizeof(char) * 14);
+	for (int n = 0; n < 13; n++)
+	{
+		a[n] = x[0];
+	}
+
+	a[13] = 0;
+
+	while (q_paixu(x, 100, a, 3))
+	{
+		;
+	}
+}
