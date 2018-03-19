87e37ca804364f0cd02754ce196b065ce3db9b51
lxs0401 <fengod0401@gmail.com>
2018-03-19 16:17:53 +0800

    add

--- 部分算法/快速幂.c
+++ 部分算法/快速幂.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+typedef long long ll;
 ll f(ll a,ll b,ll n){
   int t,y;
   t=1; y=a;
@@ -6,4 +8,12 @@ ll f(ll a,ll b,ll n){
     y=y*y%n; b=b>>1;
   }
   return t;
+}
+ll pow_mod(ll a,ll b)
+{
+  return (b % 2 == 1 ? a : 1) * (b / 2 == 0 ? 1 : pow_mod(a*a,b / 2));
+}
+int main ()
+{
+  printf("%d",pow_mod(2,3));
 }
